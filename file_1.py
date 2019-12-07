be630d299a0279084516e379eb08fa983841f1e6
Женя <evgeniy.zharov.2002@mail.ru>
2019-12-07 23:42:36 +0300

    PyQt5_Lesson_1

--- file_1.py
+++ file_1.py
@@ -1,25 +1,17 @@
-import pygame
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
 
 
-number = input()
-w, hue = int(number.split()[0]), int(number.split()[1])
-h = int(255 / 360 * hue)
-pygame.init()
-screen = pygame.display.set_mode((300, 300))
-running = True
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (int(h * 0.75), 0, 0), ((100, 100), (w, w)))
-    pygame.draw.polygon(screen, (h, 0, 0), ((100, 100), (int(100 + w * 0.5), int(100 - w * 0.5)),
-                                            (int(100 + w * 1.5), int(100 - w * 0.5)),
-                                            (100 + w, 100)))
-    pygame.draw.polygon(screen, (int(h * 0.5), 0, 0), ((100 + w, 100), (int(100 + w * 1.5), int(100 - w * 0.5)),
-                                                       (int(100 + w * 1.5), int(100 + w * 0.5)),
-                                                       (100 + w, 100 + w)))
-    pygame.display.flip()
+    w = QWidget()
+    w.resize(250, 250)
+    w.move(300, 300)
+    w.setWindowTitle('First')
+    w.show()
 
-pygame.quit()
+    sys.exit(app.exec_())
